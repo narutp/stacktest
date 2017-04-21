05220313ab34614801c6423e5f108d8a9a87341f
Narut Poovorakit <net.narut.p@gmail.com>
2017-04-21 16:25:59 +0700

    Add peek test cases

--- src/ku/util/StackTest.java
+++ src/ku/util/StackTest.java
@@ -6,16 +6,30 @@ import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Stack test for testing a stack class.
+ * 
+ * @author Narut Poovorakit
+ * 
+ * @version 21.04.2017
+ *
+ */
 public class StackTest {
 
 	private Stack stack;
 
+	/**
+	 * Create a stack by calling stackFactory to make a stack by given capacity.
+	 */
 	@Before
 	public void setUp() {
-		StackFactory.setStackType(1);
+		StackFactory.setStackType(0);
 		stack = StackFactory.makeStack(2);
 	}
 
+	/**
+	 * Test stack if it is truly empty or not.
+	 */
 	@Test
 	public void testStackIsEmpty() {
 		assertTrue(stack.isEmpty());
@@ -23,6 +37,9 @@ public class StackTest {
 		assertEquals(0, stack.size());
 	}
 	
+	/**
+	 * Test stack if it's truly full.
+	 */
 	@Test
 	public void testStackIsFull() {
 		stack.push(1);
@@ -30,6 +47,9 @@ public class StackTest {
 		assertTrue(stack.isFull());
 	}
 	
+	/**
+	 * Test the size of the stack from the beginning and after being popped.
+	 */
 	@Test
 	public void testStackSize() {
 		assertEquals(2, stack.size());
@@ -37,6 +57,9 @@ public class StackTest {
 		assertEquals(1, stack.size());
 	}
 	
+	/**
+	 * Test when push the value into the stack
+	 */
 	@Test(expected=java.lang.IllegalStateException.class)
 	public void testPushOverCapacity() {
 		stack.push(2);
@@ -90,6 +113,12 @@ public class StackTest {
 	public void testPopEmptyStack() {
 		Assume.assumeTrue(stack.isEmpty());
 		stack.pop();
+	}
+	
+	@Test
+	public void testPeekEmptyStack() {
+		Assume.assumeTrue(stack.isEmpty());
+		assertEquals(null, stack.peek());
 	}
 
 	private Stack makeStack(int capacity) {
